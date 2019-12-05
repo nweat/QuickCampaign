a7d48cfb6aea017025cb581b83c10228acef8d33
Nikki <nikkiweat@gmail.com>
2019-12-05 22:22:42 +0800

    change port to PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("hello")
 })
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log("running")
