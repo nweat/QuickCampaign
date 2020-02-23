d586bc2b96fbe097334a449f7903372965bb7c46
Nikki <nikkiweat@gmail.com>
2020-02-23 21:08:09 +0800

    - update landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,52 +7,64 @@ const Landing = () => {
       <main>
         <section className="section-about">
           <div className="u-center-text u-margin-bottom-medium">
-            <h2 className="heading-secondary">Exciting tours for adventurous people</h2>
+            <h2 className="heading-secondary">Creating a campaign has never been easier</h2>
           </div>
 
           <div className="row">
-            <h3 className="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
-            <p className="paragraph">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ducimus
-              quam nisi exercitationem omdnis earum qui.
-            </p>
+            <div className="col-1-of-3">
+              <div className="u-center-text u-margin-bottom-medium">
+                <i className="feature-box__icon icon-basic-todolist-pen"></i>
+                <span className="text"> Create </span>
+              </div>
+            </div>
+            <div className="col-1-of-3">
+              <div className="u-center-text u-margin-bottom-medium">
+                <i className="feature-box__icon icon-basic-paperplane"></i>
+                <span className="text">Send </span>
+              </div>
+            </div>
+            <div className="col-1-of-4">
+              <div className="u-center-text u-margin-bottom-medium">
+                <i className="feature-box__icon icon-basic-sheet-multiple"></i>
+                <span className="text"> Feedback </span>
+              </div>
+            </div>
 
-            <h3 className="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
-            <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla deserunt voluptatum nam.</p>
+            <div class="col">
+              <a href="#" class="btn-text">
+                Learn more &rarr;
+              </a>
+            </div>
           </div>
         </section>
 
         <section className="section-features">
           <div className="row">
-            <div className="col-1-of-4">
+            <div className="col-1-of-3">
               <div className="feature-box">
                 <i className="feature-box__icon icon-basic-world"></i>
-                <h3 className="heading-tertiary u-margin-bottom-small">Explore the world</h3>
-                <p className="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.</p>
+                <h3 className="heading-tertiary u-margin-bottom-small">Connect with Clients</h3>
+                <p className="feature-box__text">
+                  If you are just starting out or already a big company, getting the answer to that question starts with just an email
+                </p>
               </div>
             </div>
 
-            <div className="col-1-of-4">
+            <div className="col-1-of-3">
               <div className="feature-box">
-                <i className="feature-box__icon icon-basic-compass"></i>
-                <h3 className="heading-tertiary u-margin-bottom-small">Meet nature</h3>
-                <p className="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.</p>
+                <i className="feature-box__icon icon-basic-book"></i>
+                <h3 className="heading-tertiary u-margin-bottom-small">User Acceptance</h3>
+                <p className="feature-box__text">Going to launch that new product soon? QuickCampaign fits perfectly in your beta testing plans.</p>
               </div>
             </div>
 
-            <div className="col-1-of-4">
+            <div className="col-1-of-3">
               <div className="feature-box">
-                <i className="feature-box__icon icon-basic-map"></i>
-                <h3 className="heading-tertiary u-margin-bottom-small">Find your way</h3>
-                <p className="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.</p>
-              </div>
-            </div>
-
-            <div className="col-1-of-4">
-              <div className="feature-box">
-                <i className="feature-box__icon icon-basic-heart"></i>
-                <h3 className="heading-tertiary u-margin-bottom-small">Live a healthier life</h3>
-                <p className="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.</p>
+                <i className="feature-box__icon icon-basic-lightbulb"></i>
+                <h3 className="heading-tertiary u-margin-bottom-small">Plan for the future</h3>
+                <p className="feature-box__text">
+                  Want to see how your users would accept that new feature or major change? Listen to what your clients have to say first
+                </p>
               </div>
             </div>
           </div>
